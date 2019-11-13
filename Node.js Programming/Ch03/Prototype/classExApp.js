786eaa75b0fff8ef720d45241525db85c577c144
Aelleek <aelleek@gmail.com>
2019-11-13 20:07:39 +0900

    Add Ch03 CallbackFunc and Prototype Study

---
+++ Node.js Programming/Ch03/Prototype/classExApp.js
@@ -0,0 +1,10 @@
+/*
+var Person = {
+	name : null,
+	age : null,
+	walk : function(speed){
+		console.log(this.name + '가 ' + speed +'의 속도로 걷습니다.');
+	}
+}
+
+*/
---
+++ Node.js Programming/Ch03/Prototype/prototypeExApp.js
@@ -0,0 +1,39 @@
+function Person(name, age){
+	this.name = name;
+	this.age = age;
+}
+
+Person.prototype.walk = function(speed){
+	console.log(speed + 'km 속도로 걸어갑니다.');
+}
+
+var person1 = new Person('소녀시대',20);
+var person2 = new Person('걸스데이',21);
+
+
+console.log(person1.name + '의 walk() 메소드를 호출');
+person1.walk(10);
+
+
+console.log(person2.name + '의 walk() 메소드를 호출');
+person2.walk(15);
+
+
+person1.walk = function(speed){
+	console.log(speed + 'km 속도로 뛰어갑니다.');
+}
+
+console.log(person1.name + '의 walk() 메소드를 호출');
+person1.walk(10);
+
+
+console.log(person2.name + '의 walk() 메소드를 호출');
+person2.walk(15);
+
+person1.read = function(book){
+	console.log(this.name + '은 '+ book + '을 보고있습니다.');
+}
+
+person1.read('바람');
+
+//person2.read('바람바람');
